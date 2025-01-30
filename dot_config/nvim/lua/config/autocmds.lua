e31cc99754267e724e0ef2623344d9b586f7d51f
Sargun Vohra <sargunv@users.noreply.github.com>
2025-01-30 14:48:23 -0800

    title

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,7 +7,6 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- disable spell checking for all file types
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   callback = function()
