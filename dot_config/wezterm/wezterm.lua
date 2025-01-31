594a2140f7eb6f4a62e612f4f56557575d93db17
Sargun Vohra <sargunv@users.noreply.github.com>
2025-01-30 20:38:48 -0800

    nvim

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+local appearance = wezterm.gui.get_appearance()
+if appearance == "Dark" then
+  config.color_scheme = "Hemisu Dark (Gogh)"
+else
+  config.color_scheme = "Hemisu Light (Gogh)"
+end
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
+config.font_size = 14.0
+
+return config
