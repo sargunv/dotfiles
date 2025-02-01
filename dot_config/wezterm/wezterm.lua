d5b96239d03075364c56b95d2e60c899f13e5653
Sargun Vohra <sargunv@users.noreply.github.com>
2025-01-31 17:12:00 -0800

    more nvim

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,14 +2,9 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.default_prog = { '/usr/bin/env', 'zsh', '-l' }
+config.default_prog = { "/usr/bin/env", "zsh", "-l" }
 
-local appearance = wezterm.gui.get_appearance()
-if appearance == "Dark" then
-  config.color_scheme = "Hemisu Dark (Gogh)"
-else
-  config.color_scheme = "Hemisu Light (Gogh)"
-end
+config.color_scheme = "Tokyo Night (Gogh)"
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
