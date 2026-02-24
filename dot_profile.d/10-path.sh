272fff5f098c08e2f8ea32c2b89d9171996e023d
Sargun Vohra <sargunv@users.noreply.github.com>
2026-02-23 20:27:19 -0800

    remove GNU tooling PATH overrides

--- dot_profile.d/10-path.sh
+++ dot_profile.d/10-path.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# gnu tooling (macOS only, requires $HOMEBREW_PREFIX)
-if [ -n "$HOMEBREW_PREFIX" ]; then
-  GNU_PATH=$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
-  GNU_PATH=$GNU_PATH:$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin
-  GNU_PATH=$GNU_PATH:$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin
-  export PATH=$GNU_PATH:$PATH
-
-  GNU_MANPATH=$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman
-  GNU_MANPATH=$GNU_MANPATH:$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnuman
-  GNU_MANPATH=$GNU_MANPATH:$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnuman
-  export MANPATH=$GNU_MANPATH:$MANPATH
-fi
-
 # Jetbrains
 export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts"
 
