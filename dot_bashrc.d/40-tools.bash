b431650beb309ae567ec17f626fa109a0e79c1b2
Sargun Vohra <sargunv@users.noreply.github.com>
2026-02-23 22:27:52 -0800

    switch starship config to starship-multi-config with .d/ pattern

--- dot_bashrc.d/40-tools.bash
+++ dot_bashrc.d/40-tools.bash
@@ -4,7 +4,7 @@
 [ -f "/etc/profile.d/bash-preexec.sh" ] && . "/etc/profile.d/bash-preexec.sh"
 [ -f "/usr/share/bash-prexec" ] && . "/usr/share/bash-prexec"
 [ -n "$HOMEBREW_PREFIX" ] && [ -f "${HOMEBREW_PREFIX}/etc/profile.d/bash-preexec.sh" ] && . "${HOMEBREW_PREFIX}/etc/profile.d/bash-preexec.sh"
-[ "$(command -v starship)" ] && eval "$(starship init bash)"
+[ "$(command -v starship-multi-config)" ] && eval "$(starship-multi-config init bash)"
 [ "$(command -v atuin)" ] && eval "$(atuin init bash ${ATUIN_INIT_FLAGS})"
 [ "$(command -v zoxide)" ] && eval "$(zoxide init bash)"
 [ "$(command -v mise)" ] && eval "$(mise activate bash)"
