93d1a26b8919df803aeec6184a0c89bc333eb20d
Sargun Vohra <sargunv@users.noreply.github.com>
2026-02-24 00:21:55 -0800

    add lazygit nerd font config, zellij package, and disable brew auto-update in install script

--- dot_zshrc.d/05-nerd-font.zsh
+++ dot_zshrc.d/05-nerd-font.zsh
@@ -1,4 +1,9 @@
 #!/bin/zsh
 [ -n "$NO_FANCY_SHELL" ] && return
 
-command -v has-nerd-font > /dev/null && has-nerd-font && HAS_NERD_FONT=1
+LG_NERD_CONFIG=""
+if command -v has-nerd-font > /dev/null && has-nerd-font; then
+  HAS_NERD_FONT=1
+  LG_NERD_CONFIG=",$HOME/.config/lazygit/config-nerd.yml"
+fi
+export LG_CONFIG_FILE="$HOME/.config/lazygit/config.yml$LG_NERD_CONFIG${LG_CONFIG_FILE:+,$LG_CONFIG_FILE}"
